e344e7d483f058084b1aca51c6bdfe7f522701fd
umarofz <webnajottalim@gmail.com>
2022-07-06 00:35:27 +0500

    Add

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var Soqqa = prompt("Pulni kiriting");
+
+var necessarySoqqa = Math.floor(8317488.6);
+
+if (Soqqa >= necessarySoqqa) {
+  console.log("Oq yo'l, Alisher!");
+  alert("Oq yo'l, Alisher!");
+} else {
+  console.log("Alisher, ozgina sabr qilish kerak bo'lar ekan.");
+  alert("Alisher, ozgina sabr qilish kerak bo'lar ekan.");
+};
